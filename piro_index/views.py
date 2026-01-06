824082fc3c92799d08a2a4fba3a7d180e8587227
seonwoo <sunnn62@naver.com>
2026-01-06 10:02:07 +0900

    feat:video_detail 기본설정

--- piro_index/views.py
+++ piro_index/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Video, Timeline, Comment, Profile, Reply, Save, Like
+from django.db.models import Prefetch
 
-def login(request):
-    return render(request, 'piro_index/base.html')
+def video_detail(request, pk):
+
+    context = {
+        'video': target_video,
+        'comments': comments, 
+        'timelines': timelines,
+        'comment_count': comments.count(),
+    }
+
+    return render(request, 'video_detail.html', context)
